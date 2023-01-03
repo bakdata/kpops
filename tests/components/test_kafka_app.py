5a1b202494c14f1b5160af39039a72c6e6151f4b
Ramin Gharib <ramin.gharib@bakdata.com>
2023-01-03 14:20:03 +0100

    add tests to kafka app

--- tests/components/test_kafka_app.py
+++ tests/components/test_kafka_app.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from unittest.mock import MagicMock
 
 import pytest
+from pytest_mock import MockerFixture
 
 from kpops.cli.pipeline_config import PipelineConfig
 from kpops.component_handlers import ComponentHandlers
+from kpops.component_handlers.helm_wrapper.model import HelmUpgradeInstallFlags
 from kpops.components.base_components import KafkaApp
 
 DEFAULTS_PATH = Path(__file__).parent / "resources"
@@ -40,6 +42,47 @@ class TestKafkaApp:
                         "brokers": "fake-broker:9092",
                     },
                 },
+                "version": "1.2.3",
             },
         )
         assert kafka_app.app.streams.brokers == "fake-broker:9092"
+        assert kafka_app.version == "1.2.3"
+        assert kafka_app.get_helm_chart_version() == "1.2.3"
+
+    def test_should_deploy_kafka_app(
+        self, config: PipelineConfig, handlers: ComponentHandlers, mocker: MockerFixture
+    ):
+        kafka_app = KafkaApp(
+            handlers=handlers,
+            config=config,
+            **{
+                "type": "streams-app",
+                "name": "example-name",
+                "app": {
+                    "namespace": "test-namespace",
+                    "streams": {
+                        "outputTopic": "test",
+                        "brokers": "fake-broker:9092",
+                    },
+                },
+                "version": "1.2.3",
+            },
+        )
+        helm_upgrade_install = mocker.patch.object(
+            kafka_app.helm_wrapper, "upgrade_install"
+        )
+        mocker.patch.object(kafka_app, "get_helm_chart", return_value="test/test-chart")
+
+        kafka_app.deploy(True)
+
+        helm_upgrade_install.assert_called_once_with(
+            "example-name",
+            "test/test-chart",
+            True,
+            "test-namespace",
+            {
+                "namespace": "test-namespace",
+                "streams": {"brokers": "fake-broker:9092", "outputTopic": "test"},
+            },
+            HelmUpgradeInstallFlags(version="1.2.3"),
+        )
