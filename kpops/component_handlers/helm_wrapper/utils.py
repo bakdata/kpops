73d908da30e5b613e8eb7063fb47718301654819
Salomon Popp <salomon.popp@bakdata.com>
2023-01-12 14:31:32 +0100

    Fix Helm release name trimming

--- kpops/component_handlers/helm_wrapper/utils.py
+++ kpops/component_handlers/helm_wrapper/utils.py
@@ -7,10 +7,16 @@ RELEASE_NAME_MAX_LEN = 52
 
 
 def trim_release_name(name: str, suffix: str = "") -> str:
-    if len(name) + len(suffix) > RELEASE_NAME_MAX_LEN:
+    """
+    Trim Helm release name while preserving suffix.
+    :param name: The release name including optional suffix
+    :param suffix: The release suffix to preserve
+    :return: Truncated release name
+    """
+    if len(name) > RELEASE_NAME_MAX_LEN:
         new_name = name[: (RELEASE_NAME_MAX_LEN - len(suffix))] + suffix
         log.critical(
-            f"The Helm release name {name} is invalid. We shorten it to {RELEASE_NAME_MAX_LEN} characters: \n {name + suffix} --> {new_name}"
+            f"Invalid Helm release name '{name}'. Truncating to {RELEASE_NAME_MAX_LEN} characters: \n {name} --> {new_name}"
         )
         name = new_name
     return name
