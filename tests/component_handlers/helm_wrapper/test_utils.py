7aaff8e4518dcb23168eeb159135ce7c29ef466c
Salomon Popp <salomon.popp@bakdata.com>
2023-01-12 15:16:12 +0100

    Expand test

--- tests/component_handlers/helm_wrapper/test_utils.py
+++ tests/component_handlers/helm_wrapper/test_utils.py
@@ -10,7 +10,15 @@ def test_trim_release_name_with_suffix():
     assert len(name) == 52
 
 
-def test_no_need_of_trim_release_name_with_suffix():
+def test_trim_release_name_without_suffix():
+    name = trim_release_name(
+        "example-component-name-too-long-fake-fakefakefakefakefake"
+    )
+    assert name == "example-component-name-too-long-fake-fakefakefakefak"
+    assert len(name) == 52
+
+
+def test_no_trim_release_name():
     assert (
         trim_release_name("normal-name-with-no-need-of-trim-clean", suffix="-clean")
         == "normal-name-with-no-need-of-trim-clean"
