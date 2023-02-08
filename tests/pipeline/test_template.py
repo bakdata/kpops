e9296dc6a881567aff4efecb25031c33c916475e
sujuka99 <vanko_yordanov@yahoo.com>
2023-02-08 12:51:28 +0100

    lint and formatting

--- tests/pipeline/test_template.py
+++ tests/pipeline/test_template.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import yaml
-from snapshottest.module import SnapshotTest
 from typer.testing import CliRunner
 
 from kpops.cli.main import app
@@ -10,6 +8,7 @@ runner = CliRunner()
 
 RESOURCE_PATH = Path(__file__).parent / "resources"
 PIPELINE_BASE_DIR = str(RESOURCE_PATH.parent)
+
 
 class TestTemplate:
     def test_default_template_config(self):
@@ -29,7 +28,6 @@ class TestTemplate:
 
         assert result.exit_code == 0
 
-
     def test_template_config_with_flags(self):
         result = runner.invoke(
             app,
@@ -46,7 +44,7 @@ class TestTemplate:
                 "--ca-file",
                 "ca-file",
                 "--cert-file",
-                "cert-file"
+                "cert-file",
             ],
             catch_exceptions=False,
         )
