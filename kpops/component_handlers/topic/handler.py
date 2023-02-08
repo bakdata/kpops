e24eb36b12b037e4a670f8a41a76c7885e4bce88
Philipp Schirmer <philipp.schirmer@bakdata.com>
2023-02-08 09:49:20 +0100

    Clean up dry-run logging (#100)
    Fixes #87

--- kpops/component_handlers/topic/handler.py
+++ kpops/component_handlers/topic/handler.py
@@ -13,7 +13,7 @@ from kpops.component_handlers.topic.utils import (
     parse_rest_proxy_topic_config,
 )
 from kpops.components.base_components.models.to_section import TopicConfig, ToSection
-from kpops.utils.colorify import greenify, magentaify, yellowify
+from kpops.utils.colorify import greenify, magentaify
 from kpops.utils.dict_differ import Diff, DiffType, get_diff, render_diff
 
 log = logging.getLogger("KafkaTopic")
@@ -146,16 +146,14 @@ class TopicHandler:
         if partition_count == (
             topic_spec.partitions_count or int(broker_config["num.partitions"])
         ):
-            log.info(
-                yellowify(
-                    f"Topic Creation: partition count of topic {topic_name} did not change. Current partitions count {partition_count}. Updating configs."
-                )
+            log.debug(
+                f"Topic Creation: partition count of topic {topic_name} did not change. Current partitions count {partition_count}. Updating configs."
             )
         else:
             log.error(
                 f"Topic Creation: partition count of topic {topic_name} changed! Partitions count of topic {topic_name} is {partition_count}. The given partitions count {topic_spec.partitions_count}."
             )
-            exit(1)
+            exit(1)  # FIXME raise instead https://github.com/bakdata/kpops/issues/101
 
     @staticmethod
     def __check_replication_factor(
@@ -169,16 +167,14 @@ class TopicHandler:
             topic_spec.replication_factor
             or int(broker_config["default.replication.factor"])
         ):
-            log.info(
-                yellowify(
-                    f"Topic Creation: replication factor of topic {topic_name} did not change. Current replication factor {replication_factor}. Updating configs."
-                )
+            log.debug(
+                f"Topic Creation: replication factor of topic {topic_name} did not change. Current replication factor {replication_factor}. Updating configs."
             )
         else:
             log.error(
                 f"Topic Creation: replication factor of topic {topic_name} changed! Replication factor of topic {topic_name} is {replication_factor}. The given replication count {topic_spec.replication_factor}."
             )
-            exit(1)
+            exit(1)  # FIXME raise instead https://github.com/bakdata/kpops/issues/101
 
     def __dry_run_topic_deletion(self, topic_name: str) -> None:
         try:
