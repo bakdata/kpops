175dfa95354c2e2b8f0bb70469f609d164d6f261
Salomon Popp <salomon.popp@bakdata.com>
2023-02-14 11:37:20 +0100

    Refactor component handlers wrapper

--- kpops/component_handlers/__init__.py
+++ kpops/component_handlers/__init__.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from attrs import define
+
 from kpops.component_handlers.kafka_connect.kafka_connect_handler import (
     KafkaConnectHandler,
 )
@@ -11,13 +13,8 @@ if TYPE_CHECKING:
     from kpops.component_handlers.topic.handler import TopicHandler
 
 
+@define
 class ComponentHandlers:
-    def __init__(
-        self,
-        schema_handler: SchemaHandler | None,
-        connector_handler: KafkaConnectHandler,
-        topic_handler: TopicHandler,
-    ) -> None:
-        self.schema_handler = schema_handler
-        self.connector_handler = connector_handler
-        self.topic_handler = topic_handler
+    schema_handler: SchemaHandler | None
+    connector_handler: KafkaConnectHandler
+    topic_handler: TopicHandler
