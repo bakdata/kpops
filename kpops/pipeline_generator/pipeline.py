0af410676511b23ff960ee57db75ef29d5da5712
Salomon Popp <salomon.popp@bakdata.com>
2023-02-27 13:34:00 +0100

    First draft

--- kpops/pipeline_generator/pipeline.py
+++ kpops/pipeline_generator/pipeline.py
@@ -16,6 +16,11 @@ from kpops.cli.pipeline_config import PipelineConfig
 from kpops.cli.registry import Registry
 from kpops.component_handlers import ComponentHandlers
 from kpops.components.base_components.base_defaults_component import update_nested_pair
+from kpops.components.base_components.models.from_section import (
+    FromTopic,
+    InputTopicTypes,
+)
+from kpops.components.base_components.models.to_section import OutputTopicTypes
 from kpops.components.base_components.pipeline_component import PipelineComponent
 from kpops.utils.yaml_loading import load_yaml_file, substitute
 
@@ -185,7 +190,29 @@ class Pipeline:
                 enriched_component.weave_from_topics(prev.to)
             enriched_components.append(enriched_component)
 
+        # read from step
+        for enriched_component in enriched_components:
+            if enriched_component.from_:
+                for from_component in enriched_component.from_.components:
+                    component = self._find_component(from_component)
+                    if component.to:
+                        for (
+                            topic_name,
+                            topic_config,
+                        ) in component.to.topics.items():
+                            if topic_config.type is OutputTopicTypes.OUTPUT:
+                                enriched_component.apply_from_inputs(
+                                    topic_name,
+                                    FromTopic(type=InputTopicTypes.INPUT),
+                                )
+
         return enriched_components
+
+    def _find_component(self, component_name: str) -> PipelineComponent:
+        for component in self.components:
+            if component_name == component.name:
+                return component
+        raise ValueError(f"Component {component_name} not found")
 
     def enrich_component(
         self,
