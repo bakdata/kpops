a836758506c995cec4d9d57854bc42b2cb6deedc
sujuka99 <vanko_yordanov@yahoo.com>
2023-03-31 14:11:03 +0200

    fix(hooks): exit code 1 if existing changes to docs

--- gen_docs.sh
+++ gen_docs.sh
@@ -7,3 +7,6 @@ poetry run typer $scriptDir/kpops/cli/main.py utils docs --name kpops --output $
 
 echo "Fix name"
 sed -i '1s/.*/# CLI Usage/' $scriptDir/docs/docs/user/references/cli-commands.md
+
+echo "Diff docs, fail if existing changes"
+git diff --exit-code $scriptDir/docs
