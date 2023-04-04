c0f1a269a220a1ede1b2da69d3ab86a571a6ae2c
sujuka99 <vanko_yordanov@yahoo.com>
2023-04-04 11:00:15 +0200

    refactor(gen-docs): improve script

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}" )")
+output="$scriptDir"/docs/docs/user/references/cli-commands.md
 
 echo "generate CLI Usage"
-poetry run typer $scriptDir/kpops/cli/main.py utils docs --name kpops --output $scriptDir/docs/docs/user/references/cli-commands.md
+poetry run typer "$scriptDir"/kpops/cli/main.py utils docs --name kpops --output "$output"
 
 echo "Changing title to CLI Usage"
-sed -i '1s/.*/# CLI Usage/' $scriptDir/docs/docs/user/references/cli-commands.md
+sed -i '1s/.*/# CLI Usage/' "$output"
 
-if [ -n "$(git diff --exit-code $scriptDir/docs)" ]
+if [ -n "$(git diff --exit-code "$scriptDir"/docs)" ]
 then
     echo "::error::Generatable documentation is not up-to-date. Updating..." && exit 1
 else
