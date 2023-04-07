61ea88aea43c6fc0aff761da753ae57ba212b3d6
sujuka99 <vanko_yordanov@yahoo.com>
2023-04-07 14:53:18 +0200

    style: pass pre-commit

--- tests/cli/snapshots/snap_test_schema_generation.py
+++ tests/cli/snapshots/snap_test_schema_generation.py
@@ -4,10 +4,11 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
-
 snapshots = Snapshot()
 
-snapshots['TestGenSchema.test_gen_config_schema test-schema-generation'] = '''{
+snapshots[
+    "TestGenSchema.test_gen_config_schema test-schema-generation"
+] = """{
     "title": "kpops config schema",
     "$ref": "#/definitions/PipelineConfig",
     "definitions": {
@@ -237,9 +238,11 @@ snapshots['TestGenSchema.test_gen_config_schema test-schema-generation'] = '''{
         }
     }
 }
-'''
+"""
 
-snapshots['TestGenSchema.test_gen_pipeline_schema_with_custom_module test-schema-generation'] = '''{
+snapshots[
+    "TestGenSchema.test_gen_pipeline_schema_with_custom_module test-schema-generation"
+] = """{
     "title": "kpops pipeline schema",
     "type": "array",
     "items": {
@@ -1446,9 +1449,11 @@ snapshots['TestGenSchema.test_gen_pipeline_schema_with_custom_module test-schema
         }
     }
 }
-'''
+"""
 
-snapshots['TestGenSchema.test_gen_pipeline_schema_without_custom_module test-schema-generation'] = '''{
+snapshots[
+    "TestGenSchema.test_gen_pipeline_schema_without_custom_module test-schema-generation"
+] = """{
     "title": "kpops pipeline schema",
     "type": "array",
     "items": {
@@ -2535,4 +2540,4 @@ snapshots['TestGenSchema.test_gen_pipeline_schema_without_custom_module test-sch
         }
     }
 }
-'''
+"""
