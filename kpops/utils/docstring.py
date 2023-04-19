7ec30d5a81960955982b01ce3fdb7e7e393d2018
sujuka99 <vanko_yordanov@yahoo.com>
2023-04-19 17:16:22 +0200

    refactor: clean up docstring description extraction

--- kpops/utils/docstring.py
+++ kpops/utils/docstring.py
@@ -15,34 +15,35 @@ def describe_attr(name: str, docstr: str | None) -> str:
     :returns: Description of the class attribute read from the class docstring
     :rtype: str
     """
-    if not isinstance(docstr, str):
+    if docstr is None:
+        log.debug("Returned an empty string.", exc_info=True)
         return ""
 
     docstr = docstr.partition(f":param {name}:")[2]
-    docstr = _trim_description_end(docstr)
 
-    return docstr
+    return _trim_description_end(docstr)
 
 
-def describe_class(class_docstr: str | None) -> str:
+def describe_class(docstr: str | None) -> str:
     """Return class description from its docstring
 
     Excludes parameters and return definitions
 
     Works only with reStructuredText docstrings.
 
-    :param class_docstr: Class whose description is to be isolated or the class' docstring
-    :type class_docstr: type[BaseModel], str, None
-    :returns: Class description taken from the class' docstring
+    :param docstr: The class' docstring
+    :type docstr: str, None
+    :returns: Class description taken from its docstiirng
     :rtype: str
     """
+    if docstr is None:
+        log.debug("Returned an empty string.", exc_info=True)
+        return ""
 
-    class_docstr = _trim_description_end(class_docstr)
-
-    return class_docstr
+    return _trim_description_end(docstr)
 
 
-def _trim_description_end(desc: str | None) -> str:
+def _trim_description_end(desc: str) -> str:
     """Remove the unwanted text that comes after a description in a docstring
 
     A description is defined here as a string of text written in natural language.
@@ -51,25 +52,19 @@ def _trim_description_end(desc: str | None) -> str:
     Works only with reStructuredText docstrings.
 
     :param desc: Description to be isolated, only the end will be trimmed
-    :type desc: str, None
+    :type desc: str
     :returns: Isolated description
     :rtype: str
     """
-    try:
-        if not isinstance(desc, str):
-            raise ValueError("Parameter `desc` must be a string.")
-        end_index = len(desc)
-    except (ValueError, TypeError):
-        log.debug("Returned an empty string.", exc_info=True)
-        return ""
-
-    desc_enders: list[str] = [
+    desc_enders: list = [
         ":param ",
         ":type ",
         ":return:",
         ":rtype:",
         "defaults to ",
     ]
+
+    end_index = len(desc)
 
     for desc_ender in desc_enders:
         if (desc_ender in desc) and (desc.index(desc_ender) < end_index):
