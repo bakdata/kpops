ab8355dfe67065f3f5943a35825588dfee4c3b57
Ivan Yordanov <i.p.yordanov@proton.me>
2023-05-02 16:53:43 +0200

    Rewrite bash pre-commit hooks in Python (#207)

---
+++ hooks/run_mypy.py
@@ -0,0 +1,21 @@
+"""Runs mypy on kpops, tests and the pre-commit hooks
+
+info: https://jaredkhan.com/blog/mypy-pre-commit
+"""
+from sys import stderr, stdout
+
+from mypy import api
+
+from hooks import cd
+
+# Args should contain the args that the equivalent
+# call to mypy from the shell would contain
+args = ["--pretty", "kpops", "tests", "hooks"]
+with cd():
+    result = api.run(args)
+# Print normal_report to stdout
+print(result[0], file=stdout)
+# Print error_report to stderr
+print(result[1], file=stderr)
+# Exit with the exit code that mypy returns
+exit(result[2])
