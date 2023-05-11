12640bc9d946ca1e8f256ce40329493620d03da9
sujuka99 <vanko_yordanov@yahoo.com>
2023-05-11 18:19:26 +0200

    refactor: Don't substitute in kafka App

--- kpops/components/base_components/kafka_app.py
+++ kpops/components/base_components/kafka_app.py
@@ -103,14 +103,6 @@ class KafkaApp(KubernetesApp):
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        # self.app.streams.brokers = substitute(
-        #     self.app.streams.brokers, {"broker": self.config.broker}
-        # )
-        # if self.app.streams.schema_registry_url:
-        #     self.app.streams.schema_registry_url = substitute(
-        #         self.app.streams.schema_registry_url,
-        #         {"schema_registry_url": self.config.schema_registry_url},
-        #     )
 
     @property
     def clean_up_helm_chart(self) -> str:
