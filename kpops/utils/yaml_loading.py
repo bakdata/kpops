88c252e97865439a8148751ca499da00bacab3ec
sujuka99 <vanko_yordanov@yahoo.com>
2023-05-12 16:00:10 +0200

    feat: Add function that substitutes all placeholders

--- kpops/utils/yaml_loading.py
+++ kpops/utils/yaml_loading.py
@@ -35,3 +35,37 @@ def substitute(input: str, substitution: Mapping[str, Any] | None = None) -> str
     if not substitution:
         return input
     return Template(input).safe_substitute(**substitution)
+
+
+def substitute_nested(input: str, **kwargs) -> str:
+    """Allow for multiple substitutions to be passed.
+
+    Will make as many passes as needed to substitute all possible placeholders.
+
+    HINT: If :param input: is a ``Mapping`` that you converted into ``str``,
+    You pass it as a string, and as a ``Mapping`` to enable self-reference.
+
+    :Example:
+
+    >>> substitution = {
+            "a": "0",
+            "b": "${a}",
+            "c": "${b}",
+            "d": "${a}",
+        }
+    >>> input = "${a}, ${b}, ${c}, ${d}"
+    >>> print("Substituted string: " + substitute_nested(input, **substitution))
+    0, 0, 0, 0
+    
+    :param input: The raw input containing $-placeholders
+    :type input: str
+    :param **kwargs: list of Mappings
+    :return: Substituted input string
+    :rtype: str
+    """
+    if not {**kwargs}:
+        return input
+    old_str, new_str = "", substitute(input, {**kwargs})
+    while old_str != new_str:
+        old_str, new_str = new_str, substitute(new_str, {**kwargs})
+    return old_str
