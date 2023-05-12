fbdda5a963485b2c2bbc3e1be9937f8607ae7cb4
sujuka99 <vanko_yordanov@yahoo.com>
2023-05-12 12:06:34 +0200

    refactor: delete unused method

--- kpops/components/base_components/kafka_connector.py
+++ kpops/components/base_components/kafka_connector.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 import logging
 from abc import ABC
 from functools import cached_property
@@ -91,7 +90,6 @@ class KafkaConnector(PipelineComponent, ABC):
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        # self.prepare_connector_config()
 
     @cached_property
     def helm(self) -> Helm:
@@ -122,15 +120,6 @@ class KafkaConnector(PipelineComponent, ABC):
     def kafka_connect_resetter_chart(self) -> str:
         """Resetter chart for this component"""
         return f"{self.repo_config.repository_name}/kafka-connect-resetter"
-
-    # TODO: Forgot to refactor before deleting `substitute_component_variables`, fix!
-    def prepare_connector_config(self) -> None:
-        """Substitute component-related variables in config"""
-        substituted_config = self.substitute_component_variables(
-            json.dumps(self.app.dict())
-        )
-        out: dict = json.loads(substituted_config)
-        self.app = KafkaConnectConfig(**out)
 
     @override
     def deploy(self, dry_run: bool) -> None:
