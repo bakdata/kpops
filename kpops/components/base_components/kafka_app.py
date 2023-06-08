850ba4ff3f3e755a98456e5d22593b74c7b1546d
Salomon Popp <salomon.popp@bakdata.com>
2023-06-08 17:25:04 +0200

    Fix optional

--- kpops/components/base_components/kafka_app.py
+++ kpops/components/base_components/kafka_app.py
@@ -96,7 +96,7 @@ class KafkaApp(KubernetesApp):
         ),
         description=describe_attr("repo_config", __doc__),
     )
-    version: str = Field(
+    version: str | None = Field(
         default="2.9.0",
         description=describe_attr("version", __doc__),
     )
