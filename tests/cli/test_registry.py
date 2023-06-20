b6efb756265c55dae20c9aff9472cb5f1d45a6df
Salomon Popp <salomon.popp@bakdata.com>
2023-06-20 15:34:54 +0200

    Fix import from external module (#256)

--- tests/cli/test_registry.py
+++ tests/cli/test_registry.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import pytest
-from pydantic import BaseModel
 
 from kpops.cli.registry import ClassNotFoundError, Registry, _find_classes, find_class
+from kpops.component_handlers.schema_handler.schema_provider import SchemaProvider
 from kpops.components.base_components.pipeline_component import PipelineComponent
+from tests.cli.resources.module import CustomSchemaProvider
 
 
 class SubComponent(PipelineComponent):
@@ -23,16 +24,35 @@ MODULE = SubComponent.__module__
 
 
 def test_find_classes():
-    gen = _find_classes(MODULE, BaseModel)
+    gen = _find_classes(MODULE, PipelineComponent)
     assert next(gen) is SubComponent
     assert next(gen) is SubSubComponent
     with pytest.raises(StopIteration):
         next(gen)
 
 
+def test_find_builtin_classes():
+    components = [
+        class_.__name__
+        for class_ in _find_classes("kpops.components", PipelineComponent)
+    ]
+    assert len(components) == 8
+    assert components == [
+        "KafkaApp",
+        "KafkaConnector",
+        "KafkaSinkConnector",
+        "KafkaSourceConnector",
+        "KubernetesApp",
+        "PipelineComponent",
+        "ProducerApp",
+        "StreamsApp",
+    ]
+
+
 def test_find_class():
     assert find_class(MODULE, SubComponent) is SubComponent
     assert find_class(MODULE, PipelineComponent) is SubComponent
+    assert find_class(MODULE, SchemaProvider) is CustomSchemaProvider
     with pytest.raises(ClassNotFoundError):
         find_class(MODULE, dict)
 
