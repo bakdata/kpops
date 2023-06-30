8d2ba9fd2e2e2a224c0251fb2222e5c0a3806d19
sujuka99 <vanko_yordanov@yahoo.com>
2023-06-30 12:26:06 +0200

    feat: add lazy component analysis

--- hooks/gen_docs.py
+++ hooks/gen_docs.py
@@ -6,18 +6,21 @@ from pathlib import Path
 from hooks import PATH_ROOT
 from kpops.cli.registry import _find_classes
 from kpops.components.base_components.pipeline_component import PipelineComponent
+from kpops.utils.yaml_loading import load_yaml_file
+import yaml
 
 PATH_KPOPS_MAIN = PATH_ROOT / "kpops/cli/main.py"
 PATH_CLI_COMMANDS_DOC = PATH_ROOT / "docs/docs/user/references/cli-commands.md"
 PATH_DOCS_RESOURCES = PATH_ROOT / "docs/docs/resources"
 PATH_DOCS_PIPELINE_COMPONENTS = PATH_DOCS_RESOURCES / "pipeline-components"
+PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES = PATH_DOCS_PIPELINE_COMPONENTS / "dependencies"
 KPOPS_COMPONENTS = tuple(_find_classes("kpops.components", PipelineComponent))
-INHERITANCE_REF: dict = {
+KPOPS_COMPONENTS_INHERITANCE_REF: dict = {
     component.get_component_type(): component.__base__.get_component_type()
     for component in KPOPS_COMPONENTS
 }
-COMPONENTS_FIELDS = {
-    component.get_component_type(): component.__fields__.keys()
+KPOPS_COMPONENTS_FIELDS = {
+    component.get_component_type(): set(component.__fields__.keys()) # set to make it pickleable
     for component in KPOPS_COMPONENTS
 }
 SECTIONS_ORDER = [
@@ -51,20 +54,20 @@ def get_sections(
     :returns: The applicable sections
     """
     component_sections: list[str] = []
-    for target_section in COMPONENTS_FIELDS[component_name]:
+    for target_section in KPOPS_COMPONENTS_FIELDS[component_name]:
         component = component_name
         section = target_section + "-" + component + ".yaml"
         if section in sections:
             component_sections.append(section)
         elif (
-            not include_inherited and INHERITANCE_REF[component] == "pipeline-component"
+            not include_inherited and KPOPS_COMPONENTS_INHERITANCE_REF[component] == "pipeline-component"
         ):
             section = target_section + ".yaml"
             if section in sections:
                 component_sections.append(section)
         elif include_inherited:
             while component != "pipeline-component":
-                component = INHERITANCE_REF[component]
+                component = KPOPS_COMPONENTS_INHERITANCE_REF[component]
                 section = target_section + "-" + component + ".yaml"
                 if section in sections:
                     component_sections.append(section)
@@ -106,23 +109,57 @@ def component_section_position_in_definition(key: str) -> int:
             return SECTIONS_ORDER.index(section)
     return 0
 
+def check_for_changes_in_kpops_component_structure():
+    try:
+        kpops_structure = load_yaml_file(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "kpops_structure.yaml")
+    except:
+        kpops_structure = None
+    kpops_new_structure = {
+        "kpops_components_inheritance_ref": KPOPS_COMPONENTS_INHERITANCE_REF,
+        "kpops_components_fields": KPOPS_COMPONENTS_FIELDS
+    }
+    if kpops_new_structure != kpops_structure:
+        with open(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "kpops_structure.yaml", "w+") as f:
+            yaml.dump(kpops_new_structure, f)
+        print("EPIC FAIL")
+        return True
+    return False
 
 components_definition_sections = os.listdir(PATH_DOCS_PIPELINE_COMPONENTS / "sections")
 pipeline_component_file_names = os.listdir(PATH_DOCS_PIPELINE_COMPONENTS / "headers")
-pipeline_component_defaults = os.listdir(
-    PATH_DOCS_RESOURCES / "pipeline-defaults/headers"
-)
+pipeline_component_defaults = os.listdir(PATH_DOCS_RESOURCES / "pipeline-defaults/headers")
 
+some_flag = check_for_changes_in_kpops_component_structure()
+try:
+    pipeline_component_dependencies: dict[str, list[str]] = load_yaml_file(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "pipeline_component_dependencies.yaml")
+    defaults_pipeline_component_dependencies: dict[str, list[str]] = load_yaml_file(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "defaults_pipeline_component_dependencies.yaml")
+except:
+    some_flag = True
+
+if some_flag:
+    with open(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "pipeline_component_dependencies.yaml", "w+") as f:
+        f.write("")
+    with open(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "defaults_pipeline_component_dependencies.yaml", "w+") as f:
+        f.write("")
 
 for component_file_name in pipeline_component_file_names:
     component_name = component_file_name.removesuffix(".yaml")
     component_defaults_name = "defaults-" + component_file_name
-    component_sections = get_sections(
-        component_name, components_definition_sections, True
-    )
-    component_sections_not_inheritted = get_sections(
-        component_name, components_definition_sections
-    )
+
+    if some_flag:
+        component_sections = get_sections(
+            component_name, components_definition_sections, True
+        )
+        component_sections_not_inheritted = get_sections(
+            component_name, components_definition_sections
+        )
+        with open(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "pipeline_component_dependencies.yaml", "a") as f:
+            yaml.dump({component_file_name: component_sections}, f)
+        with open(PATH_DOCS_PIPELINE_COMPONENTS_DEPENDENCIES / "defaults_pipeline_component_dependencies.yaml", "a") as f:
+            yaml.dump({component_file_name: component_sections_not_inheritted}, f)
+    else:
+        component_sections = pipeline_component_dependencies[component_file_name]
+        component_sections_not_inheritted = defaults_pipeline_component_dependencies[component_file_name]
 
     defaults_sections_paths = [
         PATH_DOCS_RESOURCES / "pipeline-defaults/headers" / component_defaults_name
