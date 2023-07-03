34faa8c3a48ab6ccf3a20f9a5bb47266d2ea6210
sujuka99 <vanko_yordanov@yahoo.com>
2023-07-03 20:11:35 +0200

    ci: auto-update editor-integration example

--- hooks/gen_docs.py
+++ hooks/gen_docs.py
@@ -2,9 +2,25 @@
 import subprocess
 
 from hooks import PATH_ROOT
+from kpops import __version__
+from kpops.utils.yaml_loading import substitute
 
 PATH_KPOPS_MAIN = PATH_ROOT / "kpops/cli/main.py"
 PATH_CLI_COMMANDS_DOC = PATH_ROOT / "docs/docs/user/references/cli-commands.md"
+PATH_DOCS_RESOURCES = PATH_ROOT / "docs/docs/resources"
+
+#####################
+# EXAMPLES          #
+#####################
+
+# Keep the example editor-integration settings up to date.
+with open(PATH_DOCS_RESOURCES / "editor_integration/settings_template.json", "r") as template:
+    with open(PATH_DOCS_RESOURCES / "editor_integration/settings.json", "w+") as settings:
+        settings.write(substitute(template.read(), {"kpops_version": ".".join(__version__.split(".")[:2])}))
+
+#####################
+# CLI-USAGE         #
+#####################
 
 # Run typer-cli on kpops to generate doc on CLI usage
 # TODO: try to use typer_cli.main.docs here instead
@@ -26,3 +42,6 @@ with open(PATH_CLI_COMMANDS_DOC, "r") as f:
 text[0] = "# CLI Usage\n"
 with open(PATH_CLI_COMMANDS_DOC, "w") as f:
     f.writelines(text)
+
+
+
