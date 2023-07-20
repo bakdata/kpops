082fc9d8af6d8d92a153e2bd4977c9bbe1d53c92
Ramin Gharib <ramin.gharib@bakdata.com>
2023-07-20 16:03:59 +0200

    Add flag to exclude pipeline steps

--- tests/cli/test_pipeline_steps.py
+++ tests/cli/test_pipeline_steps.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import cast
 from unittest.mock import patch
 
-from kpops.cli.main import get_steps_to_apply
+from kpops.cli.main import FilterType, get_steps_to_apply
 from kpops.pipeline_generator.pipeline import Pipeline
 
 PREFIX = "example-prefix-"
@@ -30,7 +30,9 @@ def tests_filter_steps_to_apply(log_info):
             return iter(self.components)
 
     pipeline = cast(Pipeline, TestPipeline())
-    filtered_steps = get_steps_to_apply(pipeline, steps="example2,example3")
+    filtered_steps = get_steps_to_apply(
+        pipeline, "example2,example3", FilterType.INCLUDE
+    )
 
     assert len(filtered_steps) == 2
     assert test_component_2 in filtered_steps
@@ -39,11 +41,53 @@ def tests_filter_steps_to_apply(log_info):
     assert log_info.call_count == 2
     log_info.assert_any_call("KPOPS_PIPELINE_STEPS is defined.")
     log_info.assert_any_call(
-        "Executing only on the following steps: ['example2', 'example3'], \n ignoring ['example1']"
+        "Including only on the following steps: ['example2', 'example3'], \n ignoring ['example1']"
     )
 
-    filtered_steps = get_steps_to_apply(pipeline, steps=None)
+    filtered_steps = get_steps_to_apply(pipeline, None, FilterType.INCLUDE)
     assert len(filtered_steps) == 3
 
-    filtered_steps = get_steps_to_apply(pipeline, steps="")
+    filtered_steps = get_steps_to_apply(pipeline, "", FilterType.INCLUDE)
     assert len(filtered_steps) == 3
+
+
+@patch("kpops.cli.main.log.info")
+def tests_filter_steps_to_exclude(log_info):
+    @dataclass
+    class TestComponent:
+        name: str
+        prefix: str = PREFIX
+
+    test_component_1 = TestComponent(PREFIX + "example1")
+    test_component_2 = TestComponent(PREFIX + "example2")
+    test_component_3 = TestComponent(PREFIX + "example3")
+
+    class TestPipeline:
+        components = [
+            test_component_1,
+            test_component_2,
+            test_component_3,
+        ]
+
+        def __iter__(self):
+            return iter(self.components)
+
+    pipeline = cast(Pipeline, TestPipeline())
+    filtered_steps = get_steps_to_apply(
+        pipeline, "example2,example3", FilterType.EXCLUDE
+    )
+
+    assert len(filtered_steps) == 1
+    assert test_component_1 in filtered_steps
+
+    assert log_info.call_count == 2
+    log_info.assert_any_call("KPOPS_PIPELINE_STEPS is defined.")
+    log_info.assert_any_call(
+        "Including only on the following steps: ['example1'], \n ignoring ['example2', 'example3']"
+    )
+
+    filtered_steps = get_steps_to_apply(pipeline, None, FilterType.EXCLUDE)
+    assert len(filtered_steps) == 0
+
+    filtered_steps = get_steps_to_apply(pipeline, "", FilterType.EXCLUDE)
+    assert len(filtered_steps) == 0
