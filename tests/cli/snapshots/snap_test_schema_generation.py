2dda64f0bb6eb5b2c28aa2838bcf1af6ef3c0047
Salomon Popp <salomon.popp@bakdata.com>
2023-08-09 14:25:06 +0200

    Update snapshot

--- tests/cli/snapshots/snap_test_schema_generation.py
+++ tests/cli/snapshots/snap_test_schema_generation.py
@@ -297,6 +297,9 @@ snapshots['TestGenSchema.test_gen_pipeline_schema_only_custom_module test-schema
             "description": "Holds multiple output topics",
             "properties": {
                 "models": {
+                    "additionalProperties": {
+                        "type": "string"
+                    },
                     "default": {},
                     "description": "Data models",
                     "title": "Models",
