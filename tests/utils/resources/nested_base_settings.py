242be4cafe19e8fb37a4adad767fd4520df4b5ae
Ivan Yordanov <i.p.yordanov@proton.me>
2023-09-22 13:52:03 +0300

    chore: lint and format

--- tests/utils/resources/nested_base_settings.py
+++ tests/utils/resources/nested_base_settings.py
@@ -1,9 +1,10 @@
 from pydantic import BaseSettings, Field
 
+
 class NestedSettings(BaseSettings):
     attr: str = Field("attr")
 
-class ParentSettings(BaseSettings):
 
+class ParentSettings(BaseSettings):
     not_nested_field: str = Field("not_nested_field")
     nested_field: NestedSettings = Field(...)
