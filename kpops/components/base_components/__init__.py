1bf6c5ff3ec1b16013d949ce64f4c6b82fe8576c
Salomon Popp <salomon.popp@bakdata.com>
2023-10-12 17:08:32 +0200

    Add HelmApp to kpops.components

--- kpops/components/base_components/__init__.py
+++ kpops/components/base_components/__init__.py
@@ -1,3 +1,4 @@
+from kpops.components.base_components.helm_app import HelmApp
 from kpops.components.base_components.kafka_app import KafkaApp
 from kpops.components.base_components.kafka_connector import (
     KafkaSinkConnector,
@@ -7,6 +8,7 @@ from kpops.components.base_components.kubernetes_app import KubernetesApp
 from kpops.components.base_components.pipeline_component import PipelineComponent
 
 __all__ = (
+    "HelmApp",
     "KafkaApp",
     "KafkaSinkConnector",
     "KafkaSourceConnector",
