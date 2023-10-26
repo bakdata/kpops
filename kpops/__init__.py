c7ed17b09c51a5d1bd721064df2d778b443fa6bc
Salomon Popp <salomon.popp@bakdata.com>
2023-10-26 18:46:32 +0200

    Expose `render` in public API

--- kpops/__init__.py
+++ kpops/__init__.py
@@ -1,10 +1,11 @@
 __version__ = "2.0.11"
 
 # export public API functions
-from kpops.cli.main import clean, deploy, destroy, generate, reset
+from kpops.cli.main import clean, deploy, destroy, generate, render, reset
 
 __all__ = (
     "generate",
+    "render",
     "deploy",
     "destroy",
     "reset",
