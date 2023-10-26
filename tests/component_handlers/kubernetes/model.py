fb46ff04133585673fe99b6dfa77a2e9f99c84aa
Salomon Popp <salomon.popp@bakdata.com>
2023-10-26 13:35:16 +0200

    Refactor test as parametrized

--- tests/component_handlers/kubernetes/model.py
+++ tests/component_handlers/kubernetes/model.py
@@ -1,25 +1,35 @@
 from textwrap import dedent
 
+import pytest
+
 from kpops.component_handlers.kubernetes.model import KubernetesManifest
 
 
 class TestKubernetesManifest:
-    def test_from_yaml(self):
-        helm_template = dedent(
-            """
-            ---
-            # Source: chart/templates/test2.yaml
-            apiVersion: v1
-            kind: ServiceAccount
-            metadata:
-                labels:
-                    foo: bar
-            """
-        )
-
-        manifest = KubernetesManifest.from_yaml(helm_template)
-        assert manifest == {
-            "apiVersion": "v1",
-            "kind": "ServiceAccount",
-            "metadata": {"labels": {"foo": "bar"}},
-        }
+    @pytest.mark.parametrize(
+        ("helm_template", "expected_manifest"),
+        [
+            pytest.param(
+                dedent(
+                    """
+                    ---
+                    # Source: chart/templates/test2.yaml
+                    apiVersion: v1
+                    kind: ServiceAccount
+                    metadata:
+                        labels:
+                            foo: bar
+                    """
+                ),
+                KubernetesManifest(
+                    {
+                        "apiVersion": "v1",
+                        "kind": "ServiceAccount",
+                        "metadata": {"labels": {"foo": "bar"}},
+                    }
+                ),
+            )
+        ],
+    )
+    def test_from_yaml(self, helm_template: str, expected_manifest: KubernetesManifest):
+        assert KubernetesManifest.from_yaml(helm_template) == expected_manifest
