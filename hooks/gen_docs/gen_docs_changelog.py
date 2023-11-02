de1346b4377c058213fe151a5f5aa870008a65c1
Ivan Yordanov <i.p.yordanov@proton.me>
2023-11-02 11:45:39 +0200

    chore: lint and format

--- hooks/gen_docs/gen_docs_changelog.py
+++ hooks/gen_docs/gen_docs_changelog.py
@@ -1,6 +1,6 @@
 """Copies ``CHANGELOG.md`` from root to the docs."""
-from hooks.gen_docs.utils import copy_file
 from hooks import ROOT
+from hooks.gen_docs.utils import copy_file
 
 SOURCE = ROOT / "CHANGELOG.md"
 DEST = ROOT / "docs/docs/user/changelog.md"
