dfc5824bdba481a51a0232e710a699cc8e50807e
Ivan Yordanov <i.p.yordanov@proton.me>
2023-11-02 16:26:48 +0200

    chore: delete unused files

--- hooks/gen_docs/gen_docs_changelog.py
+++
@@ -1,8 +0,0 @@
-"""Copies ``CHANGELOG.md`` from root to the docs."""
-from hooks import ROOT
-from hooks.gen_docs.utils import copy_file
-
-SOURCE = ROOT / "CHANGELOG.md"
-DEST = ROOT / "docs/docs/user/changelog.md"
-
-copy_file(SOURCE, DEST)
--- hooks/gen_docs/gen_docs_contributing.py
+++
@@ -1,8 +0,0 @@
-"""Copies ``CONTRIBUTING.md`` from root to the docs."""
-from hooks import ROOT
-from hooks.gen_docs.utils import copy_file
-
-SOURCE = ROOT / "CONTRIBUTING.md"
-DEST = ROOT / "docs/docs/developer/contributing.md"
-
-copy_file(SOURCE, DEST)
