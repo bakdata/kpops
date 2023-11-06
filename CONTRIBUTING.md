adb1663a04d61a991aa33d53f5a4acc0437895a5
Ivan Yordanov <i.p.yordanov@proton.me>
2023-11-06 14:32:01 +0200

    Replace black with ruff (#365)

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+docs/docs/developer/contributing.md
--- kpops/cli/main.py
+++ kpops/cli/main.py
@@ -271,9 +271,7 @@ def generate(
     return pipeline
 
 
-@app.command(
-    help="Deploy pipeline steps"
-)  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
+@app.command(help="Deploy pipeline steps")  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
 def deploy(
     pipeline_path: Path = PIPELINE_PATH_ARG,
     components_module: Optional[str] = COMPONENTS_MODULES,
@@ -296,9 +294,7 @@ def deploy(
         component.deploy(dry_run)
 
 
-@app.command(
-    help="Destroy pipeline steps"
-)  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
+@app.command(help="Destroy pipeline steps")  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
 def destroy(
     pipeline_path: Path = PIPELINE_PATH_ARG,
     components_module: Optional[str] = COMPONENTS_MODULES,
@@ -320,9 +316,7 @@ def destroy(
         component.destroy(dry_run)
 
 
-@app.command(
-    help="Reset pipeline steps"
-)  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
+@app.command(help="Reset pipeline steps")  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
 def reset(
     pipeline_path: Path = PIPELINE_PATH_ARG,
     components_module: Optional[str] = COMPONENTS_MODULES,
@@ -345,9 +339,7 @@ def reset(
         component.reset(dry_run)
 
 
-@app.command(
-    help="Clean pipeline steps"
-)  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
+@app.command(help="Clean pipeline steps")  # pyright: ignore[reportGeneralTypeIssues] https://github.com/rec/dtyper/issues/8
 def clean(
     pipeline_path: Path = PIPELINE_PATH_ARG,
     components_module: Optional[str] = COMPONENTS_MODULES,
