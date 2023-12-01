b1d1ffb9a864c5371cdf7469213ed45c5de76fb9
Ivan Yordanov <i.p.yordanov@proton.me>
2023-12-01 18:11:03 +0200

    tests: Add fixture that clears the yaml loader cache

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+from contextlib import suppress
 import os
 from collections.abc import Iterator
 from unittest import mock
 
 import pytest
+
+from kpops.utils.yaml_loading import load_yaml_file
 
 
 @pytest.fixture()
@@ -14,3 +17,8 @@ def mock_env() -> Iterator[os._Environ[str]]:
     """
     with mock.patch.dict(os.environ, clear=True):
         yield os.environ
+
+@pytest.fixture()
+def load_yaml_file_clear_cache() -> Iterator[None]:
+    yield
+    load_yaml_file.cache.clear()
