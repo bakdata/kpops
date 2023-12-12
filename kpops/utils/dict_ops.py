8ddb8d8626c2b72e25353fba87b4ca853dd2d619
Ivan Yordanov <i.p.yordanov@proton.me>
2023-12-12 11:37:05 +0200

    Fix documentation generation (#384)
    closes #366

--- kpops/utils/dict_ops.py
+++ kpops/utils/dict_ops.py
@@ -66,7 +66,7 @@ def flatten_mapping(
         if prefix:
             key = prefix + separator + key
         if isinstance(value, Mapping):
-            nested_mapping = flatten_mapping(value, key)
+            nested_mapping = flatten_mapping(value, key, separator)
             top = update_nested_pair(top, nested_mapping)
         else:
             top[key] = value
@@ -77,7 +77,8 @@ def generate_substitution(
     input: dict,
     prefix: str | None = None,
     existing_substitution: dict | None = None,
-) -> dict:
+    separator: str | None = None,
+) -> dict[Any, Any]:
     """Generate a complete substitution dict from a given dict.
 
     Finds all attributes that belong to a model and expands them to create
@@ -88,4 +89,10 @@ def generate_substitution(
     :param substitution: existing substitution to include
     :returns: Substitution dict of all variables related to the model.
     """
-    return update_nested(existing_substitution or {}, flatten_mapping(input, prefix))
+    if separator is None:
+        return update_nested(
+            existing_substitution or {}, flatten_mapping(input, prefix)
+        )
+    return update_nested(
+        existing_substitution or {}, flatten_mapping(input, prefix, separator)
+    )
