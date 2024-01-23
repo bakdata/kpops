90288cf37855513be977e37bc3ddb53fb4aea559
Salomon Popp <salomon.popp@bakdata.com>
2024-01-23 11:23:29 +0100

    Refactor test

--- tests/component_handlers/helm_wrapper/test_helm_diff.py
+++ tests/component_handlers/helm_wrapper/test_helm_diff.py
@@ -1,56 +1,64 @@
 from pathlib import Path
 
+import pytest
+
 from kpops.component_handlers.helm_wrapper.helm_diff import HelmDiff
 from kpops.component_handlers.helm_wrapper.model import HelmDiffConfig, HelmTemplate
 from kpops.component_handlers.kubernetes.model import KubernetesManifest
 from kpops.utils.dict_differ import Change
 
 
-def test_diff():
-    helm_diff = HelmDiff(HelmDiffConfig())
-    templates = [HelmTemplate(Path("a.yaml"), KubernetesManifest())]
-    assert list(helm_diff.calculate_changes(templates, templates)) == [
-        Change(
-            old_value={},
-            new_value={},
-        ),
-    ]
+class TestHelmDiff:
+    @pytest.fixture()
+    def helm_diff(self) -> HelmDiff:
+        return HelmDiff(HelmDiffConfig())
 
-    # test matching corresponding template files based on their filename
-    assert list(
-        helm_diff.calculate_changes(
-            [
-                HelmTemplate(Path("a.yaml"), KubernetesManifest({"a": 1})),
-                HelmTemplate(Path("b.yaml"), KubernetesManifest({"b": 1})),
-            ],
-            [
-                HelmTemplate(Path("a.yaml"), KubernetesManifest({"a": 2})),
-                HelmTemplate(Path("c.yaml"), KubernetesManifest({"c": 1})),
-            ],
-        )
-    ) == [
-        Change(
-            old_value={"a": 1},
-            new_value={"a": 2},
-        ),
-        Change(
-            old_value={"b": 1},
-            new_value={},
-        ),
-        Change(
-            old_value={},
-            new_value={"c": 1},
-        ),
-    ]
+    def test_calculate_changes_unchanged(self, helm_diff: HelmDiff):
+        templates = [HelmTemplate(Path("a.yaml"), KubernetesManifest())]
+        assert list(helm_diff.calculate_changes(templates, templates)) == [
+            Change(
+                old_value={},
+                new_value={},
+            ),
+        ]
 
-    # test no current release
-    assert list(
-        helm_diff.calculate_changes(
-            (), [HelmTemplate(Path("a.yaml"), KubernetesManifest({"a": 1}))]
-        )
-    ) == [
-        Change(
-            old_value={},
-            new_value={"a": 1},
-        ),
-    ]
+    def test_calculate_changes_matching(self, helm_diff: HelmDiff):
+        # test matching corresponding template files based on their filename
+        assert list(
+            helm_diff.calculate_changes(
+                [
+                    HelmTemplate(Path("a.yaml"), KubernetesManifest({"a": 1})),
+                    HelmTemplate(Path("b.yaml"), KubernetesManifest({"b": 1})),
+                ],
+                [
+                    HelmTemplate(Path("a.yaml"), KubernetesManifest({"a": 2})),
+                    HelmTemplate(Path("c.yaml"), KubernetesManifest({"c": 1})),
+                ],
+            )
+        ) == [
+            Change(
+                old_value={"a": 1},
+                new_value={"a": 2},
+            ),
+            Change(
+                old_value={"b": 1},
+                new_value={},
+            ),
+            Change(
+                old_value={},
+                new_value={"c": 1},
+            ),
+        ]
+
+    def test_calculate_changes_new_release(self, helm_diff: HelmDiff):
+        # test no current release
+        assert list(
+            helm_diff.calculate_changes(
+                (), [HelmTemplate(Path("a.yaml"), KubernetesManifest({"a": 1}))]
+            )
+        ) == [
+            Change(
+                old_value={},
+                new_value={"a": 1},
+            ),
+        ]
