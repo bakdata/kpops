b071e636f33884e6526b28b73e3e1f7507d379e5
Salomon Popp <salomon.popp@bakdata.com>
2024-01-25 19:07:24 +0100

    Add test

--- tests/utils/test_pydantic.py
+++ tests/utils/test_pydantic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from kpops.utils.pydantic import to_dash
+from kpops.utils.pydantic import to_dash, to_snake
 
 
 @pytest.mark.parametrize(
@@ -12,7 +12,24 @@ from kpops.utils.pydantic import to_dash
         ("ExampleACRONYMComponent", "example-acronym-component"),
         ("ComponentWithACRONYM", "component-with-acronym"),
         ("ComponentWithDIGIT00", "component-with-digit00"),
+        ("S3Test", "s3-test"),
     ],
 )
 def test_to_dash(input: str, expected: str):
     assert to_dash(input) == expected
+
+
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    [
+        ("BaseDefaultsComponent", "base_defaults_component"),
+        ("ACRONYM", "acronym"),
+        ("ACRONYMComponent", "acronym_component"),
+        ("ExampleACRONYMComponent", "example_acronym_component"),
+        ("ComponentWithACRONYM", "component_with_acronym"),
+        ("ComponentWithDIGIT00", "component_with_digit00"),
+        ("S3Test", "s3_test"),
+    ],
+)
+def test_to_snake(input: str, expected: str):
+    assert to_snake(input) == expected
