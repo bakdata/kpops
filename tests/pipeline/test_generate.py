e7722ed424ef5ea3bb8a908f9cedbf09855a3b58
Salomon Popp <salomon.popp@bakdata.com>
2024-02-06 18:24:10 +0100

    Run the actual assertion

--- tests/pipeline/test_generate.py
+++ tests/pipeline/test_generate.py
@@ -876,6 +876,8 @@ class TestGenerate:
         assert isinstance(pipeline.components[0], KafkaSinkConnector)
         assert pipeline.components[0].name == "es-sink-connector"
         assert pipeline.components[0]._resetter.name == "es-sink-connector"
+        assert hasattr(pipeline.components[0]._resetter.app, "label")
+        assert pipeline.components[0]._resetter.app.label == "inflated-connector-name"
 
         enriched_pipeline: list = yaml.safe_load(pipeline.to_yaml())
         assert enriched_pipeline[0]["name"] == "es-sink-connector"
