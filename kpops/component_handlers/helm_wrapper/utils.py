ce3445f2172e852f2ecb9fce95d4b91b422c4c36
Salomon Popp <salomon.popp@bakdata.com>
2024-02-15 19:08:05 +0100

    Update pydocs

--- kpops/component_handlers/helm_wrapper/utils.py
+++ kpops/component_handlers/helm_wrapper/utils.py
@@ -9,7 +9,7 @@ def create_helm_release_name(name: str, suffix: str = "") -> str:
 
     :param name: The Helm release name to be shortened.
     :param suffix: The release suffix to preserve
-    :return: Trimmed + hashed version of the release name if it exceeds the Helm release character length otherwise the actual release name
+    :return: Trimmed + hashed version of the release name if it exceeds the Helm release character length, otherwise the input name
     """
     return trim(RELEASE_NAME_MAX_LEN, name, suffix)
 
@@ -17,8 +17,8 @@ def create_helm_release_name(name: str, suffix: str = "") -> str:
 def create_helm_name_override(name: str, suffix: str = "") -> str:
     """Create Helm chart `nameOverride`.
 
-    :param name: The Helm release name to be shortened.
+    :param name: The Helm name override to be shortened.
     :param suffix: The release suffix to preserve
-    :return: Trimmed + hashed version of the release name if it exceeds the Kubernetes character length otherwise the actual release name
+    :return: Trimmed + hashed version of the name override if it exceeds the Kubernetes character length, otherwise the input name
     """
     return trim(K8S_LABEL_MAX_LEN, name, suffix)
