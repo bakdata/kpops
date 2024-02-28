81a8158c443e851967327d6d0e3bf4a95f97225b
Jakob Edding <15202881+JakobEdding@users.noreply.github.com>
2024-02-28 18:50:34 +0100

    Document precedence between env vars and config.yaml

--- hooks/gen_docs/gen_docs_env_vars.py
+++ hooks/gen_docs/gen_docs_env_vars.py
@@ -35,7 +35,7 @@ PATH_CONFIG_ENV_VARS_MD_FILE = PATH_DOCS_VARIABLES / "config_env_vars.md"
 PATH_CONFIG_ENV_VARS_CSV_FILE = PATH_DOCS_VARIABLES / "temp_config_env_vars.csv"
 TITLE_CONFIG_ENV_VARS = "Global config environment variables"
 DESCRIPTION_CONFIG_ENV_VARS = (
-    "These variables are a lower priority alternative to the settings in `config.yaml`. "
+    "These variables take precedence over the settings in `config.yaml`. "
     "Variables marked as required can instead be set in the global config."
 )
 
