9405a1ac9e4319753cefd61d7ae72df731af415c
Salomon Popp <salomon.popp@bakdata.com>
2024-02-29 11:52:26 +0000

    Fix Pyright diagnostics around `typing.Self` on Python 3.10

--- kpops/component_handlers/kubernetes/model.py
+++ kpops/component_handlers/kubernetes/model.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 from collections import UserDict
 from collections.abc import Iterator
@@ -6,11 +8,6 @@ import yaml
 
 from kpops.utils.types import JsonType
 
-try:
-    from typing import Self
-except ImportError:
-    from typing_extensions import Self
-
 K8S_LABEL_MAX_LEN = 63
 
 
@@ -18,12 +15,16 @@ class KubernetesManifest(UserDict[str, JsonType]):
     """Representation of a Kubernetes API object as YAML/JSON mapping."""
 
     @classmethod
-    def from_yaml(cls, /, content: str) -> Iterator[Self]:
+    def from_yaml(
+        cls, /, content: str
+    ) -> Iterator[KubernetesManifest]:  # TODO: typing.Self for Python 3.11+
         manifests: Iterator[dict[str, JsonType]] = yaml.load_all(content, yaml.Loader)
         for manifest in manifests:
             yield cls(manifest)
 
     @classmethod
-    def from_json(cls, /, content: str) -> Self:
+    def from_json(
+        cls, /, content: str
+    ) -> KubernetesManifest:  # TODO: typing.Self for Python 3.11+
         manifest: dict[str, JsonType] = json.loads(content)
         return cls(manifest)
