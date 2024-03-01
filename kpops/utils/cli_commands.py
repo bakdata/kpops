5a57da45d9338d55e5fe06d9555bf09bb5b38367
Ivan Yordanov <i.p.yordanov@proton.me>
2024-03-01 11:59:42 +0200

    Add case for default value being a BaseModel instance

--- kpops/utils/cli_commands.py
+++ kpops/utils/cli_commands.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Any
 
 import yaml
+from pydantic import BaseModel
 from pydantic.fields import FieldInfo
 from pydantic_core import PydanticUndefined
 
@@ -10,6 +11,7 @@ from hooks.gen_docs.gen_docs_env_vars import collect_fields
 from kpops.config import KpopsConfig
 from kpops.utils.docstring import describe_object
 from kpops.utils.json import is_jsonable
+from kpops.utils.pydantic import issubclass_patched
 
 log = logging.getLogger("cli_commands_utils")
 
@@ -32,6 +34,8 @@ def extract_config_fields_for_yaml(
             elif not (required or value.default in [PydanticUndefined, Ellipsis]):
                 if is_jsonable(value.default):
                     extracted_fields[key] = value.default
+                elif issubclass_patched(value.default, BaseModel):
+                    extracted_fields[key] = value.default.model_dump(mode="json")
                 else:
                     extracted_fields[key] = str(value.default)
         else:
