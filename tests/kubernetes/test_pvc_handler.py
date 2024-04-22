9cbfc0fb90f407e230e87d69f3c33ebbf4bc133c
Ramin Gharib <ramin.gharib@bakdata.com>
2024-04-22 16:45:05 +0200

    Update files

--- tests/kubernetes/test_pvc_handler.py
+++ tests/kubernetes/test_pvc_handler.py
@@ -1,4 +1,4 @@
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 from pytest_mock import MockerFixture
@@ -13,30 +13,26 @@ def pvc_handler():
     return PVCHandler("test-app", "test-namespace")
 
 
-async def pvc_names_return_value() -> list[str]:
-    return ["test-pvc"]
+@pytest.mark.asyncio
+async def test_create(pvc_handler: PVCHandler, mocker: MockerFixture):
+    mock_load_kube_config = mocker.patch(f"{MODULE}.config.load_kube_config")
+    pvc_handler = await PVCHandler.create("test-app", "test-namespace")
+    mock_load_kube_config.assert_called_once()
+    assert isinstance(pvc_handler, PVCHandler)
+    assert pvc_handler.namespace == "test-namespace"
+    assert pvc_handler.app_name == "test-app"
 
 
 @pytest.mark.asyncio
-async def test_create(pvc_handler: PVCHandler):
-    with patch(f"{MODULE}.config.load_kube_config") as mock_load_kube_config:
-        pvc_handler = await PVCHandler.create("test-app", "test-namespace")
-        mock_load_kube_config.assert_called_once()
-        assert isinstance(pvc_handler, PVCHandler)
-        assert pvc_handler.namespace == "test-namespace"
-        assert pvc_handler.app_name == "test-app"
-
-
-@pytest.mark.asyncio
-async def test_pvc_names(pvc_handler: PVCHandler):
+async def test_pvc_names(pvc_handler: PVCHandler, mocker: MockerFixture):
     mock_pvc = MagicMock()
     mock_pvc.metadata.name = "test-pvc"
 
-    with patch(f"{MODULE}.client.CoreV1Api") as mock_core_v1_api:
-        mock_core_v1_api.return_value.list_namespaced_persistent_volume_claim.return_value.items = [
-            mock_pvc
-        ]
-        pvc_names = await pvc_handler.get_pvc_names()
+    mock_core_v1_api = mocker.patch(f"{MODULE}.client.CoreV1Api")
+    mock_core_v1_api.return_value.list_namespaced_persistent_volume_claim.return_value.items = [
+        mock_pvc
+    ]
+    pvc_names = await pvc_handler.get_pvc_names()
 
     assert pvc_names == ["test-pvc"]
 
