e3396d24a79cfcca1ab74b78fe385fcb524094cf
Salomon Popp <salomon.popp@bakdata.com>
2024-07-24 09:54:34 +0200

    Improve incomplete type hints

--- kpops/components/base_components/kafka_app.py
+++ kpops/components/base_components/kafka_app.py
@@ -58,7 +58,7 @@ class KafkaStreamsConfig(CamelCaseConfigModel, DescConfigModel):
     @pydantic.field_validator("extra_output_topics", mode="before")
     @classmethod
     def deserialize_extra_output_topics(
-        cls, extra_output_topics: Any
+        cls, extra_output_topics: dict[str, str] | Any
     ) -> dict[str, KafkaTopic] | Any:
         if isinstance(extra_output_topics, dict):
             return {
