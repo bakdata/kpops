f47af8c64a37cc41f01c52cc0a5b97cdc646434c
Ramin Gharib <ramin.gharib@bakdata.com>
2024-08-12 17:15:57 +0200

    Add support for streams-bootstrap v3 (#519)
    depends on https://github.com/bakdata/streams-bootstrap/pull/250/

---
+++ kpops/components/streams_bootstrap_v3/model.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pydantic
+from pydantic import AliasChoices, ConfigDict, Field
+
+from kpops.components.base_components.helm_app import HelmAppValues
+from kpops.components.common.topic import KafkaTopic, KafkaTopicStr
+from kpops.utils.docstring import describe_attr
+from kpops.utils.pydantic import (
+    CamelCaseConfigModel,
+    DescConfigModel,
+    exclude_by_value,
+    exclude_defaults,
+)
+
+# Source of the pattern: https://kubernetes.io/docs/concepts/containers/images/#image-names
+IMAGE_TAG_PATTERN = r"^[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}$"
+
+
+class StreamsBootstrapV3Values(HelmAppValues):
+    """Base value class for all streams bootstrap related components.
+
+    :param image_tag: Docker image tag of the streams-bootstrap app.
+    :param kafka: Kafka configuration for the streams-bootstrap app.
+    """
+
+    image_tag: str = Field(
+        default="latest",
+        pattern=IMAGE_TAG_PATTERN,
+        description=describe_attr("image_tag", __doc__),
+    )
+
+    kafka: KafkaConfig = Field(
+        default=...,
+        description=describe_attr("kafka", __doc__),
+    )
+
+
+class KafkaConfig(CamelCaseConfigModel, DescConfigModel):
+    """Kafka Streams config.
+
+    :param bootstrap_servers: Brokers
+    :param schema_registry_url: URL of the schema registry, defaults to None
+    :param labeled_output_topics: Extra output topics
+    :param output_topic: Output topic, defaults to None
+    """
+
+    bootstrap_servers: str = Field(
+        default=..., description=describe_attr("bootstrap_servers", __doc__)
+    )
+    schema_registry_url: str | None = Field(
+        default=None,
+        validation_alias=AliasChoices(
+            "schema_registry_url", "schemaRegistryUrl"
+        ),  # TODO: same for other camelcase fields, avoids duplicates during enrichment
+        description=describe_attr("schema_registry_url", __doc__),
+    )
+    labeled_output_topics: dict[str, KafkaTopicStr] = Field(
+        default={}, description=describe_attr("labeled_output_topics", __doc__)
+    )
+    output_topic: KafkaTopicStr | None = Field(
+        default=None,
+        description=describe_attr("output_topic", __doc__),
+        json_schema_extra={},
+    )
+
+    model_config = ConfigDict(extra="allow")
+
+    @pydantic.field_validator("labeled_output_topics", mode="before")
+    @classmethod
+    def deserialize_labeled_output_topics(
+        cls, labeled_output_topics: dict[str, str] | Any
+    ) -> dict[str, KafkaTopic] | Any:
+        if isinstance(labeled_output_topics, dict):
+            return {
+                label: KafkaTopic(name=topic_name)
+                for label, topic_name in labeled_output_topics.items()
+            }
+        return labeled_output_topics
+
+    @pydantic.field_serializer("labeled_output_topics")
+    def serialize_labeled_output_topics(
+        self, labeled_output_topics: dict[str, KafkaTopic]
+    ) -> dict[str, str]:
+        return {label: topic.name for label, topic in labeled_output_topics.items()}
+
+    # TODO(Ivan Yordanov): Currently hacky and potentially unsafe. Find cleaner solution
+    @pydantic.model_serializer(mode="wrap", when_used="always")
+    def serialize_model(
+        self,
+        default_serialize_handler: pydantic.SerializerFunctionWrapHandler,
+        info: pydantic.SerializationInfo,
+    ) -> dict[str, Any]:
+        return exclude_defaults(
+            self, exclude_by_value(default_serialize_handler(self), None)
+        )
---
+++ tests/components/streams_bootstrap_v3/test_producer_app.py
@@ -0,0 +1,474 @@
+import logging
+from unittest.mock import ANY, MagicMock
+
+import pytest
+from pytest_mock import MockerFixture
+
+from kpops.component_handlers import get_handlers
+from kpops.component_handlers.helm_wrapper.helm import Helm
+from kpops.component_handlers.helm_wrapper.model import HelmUpgradeInstallFlags
+from kpops.component_handlers.helm_wrapper.utils import create_helm_release_name
+from kpops.components.common.topic import (
+    KafkaTopic,
+    OutputTopicTypes,
+    TopicConfig,
+)
+from kpops.components.streams_bootstrap_v3.producer.producer_app import (
+    ProducerAppCleaner,
+    ProducerAppV3,
+)
+
+PRODUCER_APP_NAME = "test-producer-app-with-long-name-0123456789abcdefghijklmnop"
+PRODUCER_APP_FULL_NAME = "${pipeline.name}-" + PRODUCER_APP_NAME
+PRODUCER_APP_HELM_NAME_OVERRIDE = (
+    "${pipeline.name}-" + "test-producer-app-with-long-name-0123456-c4c51"
+)
+PRODUCER_APP_RELEASE_NAME = create_helm_release_name(PRODUCER_APP_FULL_NAME)
+PRODUCER_APP_CLEAN_FULL_NAME = PRODUCER_APP_FULL_NAME + "-clean"
+PRODUCER_APP_CLEAN_HELM_NAMEOVERRIDE = (
+    "${pipeline.name}-" + "test-producer-app-with-long-name-0-abc43-clean"
+)
+PRODUCER_APP_CLEAN_RELEASE_NAME = create_helm_release_name(
+    PRODUCER_APP_CLEAN_FULL_NAME, "-clean"
+)
+
+
+@pytest.mark.usefixtures("mock_env")
+class TestProducerApp:
+    def test_release_name(self):
+        assert PRODUCER_APP_CLEAN_RELEASE_NAME.endswith("-clean")
+
+    @pytest.fixture()
+    def producer_app(self) -> ProducerAppV3:
+        return ProducerAppV3(
+            name=PRODUCER_APP_NAME,
+            **{
+                "version": "3.2.1",
+                "namespace": "test-namespace",
+                "values": {
+                    "kafka": {"bootstrapServers": "fake-broker:9092"},
+                },
+                "clean_schemas": True,
+                "to": {
+                    "topics": {
+                        "producer-app-output-topic": TopicConfig(
+                            type=OutputTopicTypes.OUTPUT, partitions_count=10
+                        ),
+                    }
+                },
+            },
+        )
+
+    @pytest.fixture(autouse=True)
+    def empty_helm_get_values(self, mocker: MockerFixture) -> MagicMock:
+        return mocker.patch.object(Helm, "get_values", return_value=None)
+
+    def test_cleaner(self, producer_app: ProducerAppV3):
+        cleaner = producer_app._cleaner
+        assert isinstance(cleaner, ProducerAppCleaner)
+        assert not hasattr(cleaner, "_cleaner")
+
+    def test_cleaner_inheritance(self, producer_app: ProducerAppV3):
+        assert producer_app._cleaner.values == producer_app.values
+
+    def test_cleaner_helm_release_name(self, producer_app: ProducerAppV3):
+        assert (
+            producer_app._cleaner.helm_release_name
+            == "${pipeline.name}-test-producer-app-with-l-abc43-clean"
+        )
+
+    def test_cleaner_helm_name_override(self, producer_app: ProducerAppV3):
+        assert (
+            producer_app._cleaner.to_helm_values()["nameOverride"]
+            == PRODUCER_APP_CLEAN_HELM_NAMEOVERRIDE
+        )
+
+    def test_output_topics(self):
+        producer_app = ProducerAppV3(
+            name=PRODUCER_APP_NAME,
+            **{
+                "namespace": "test-namespace",
+                "values": {
+                    "namespace": "test-namespace",
+                    "kafka": {"bootstrapServers": "fake-broker:9092"},
+                },
+                "to": {
+                    "topics": {
+                        "producer-app-output-topic": TopicConfig(
+                            type=OutputTopicTypes.OUTPUT, partitions_count=10
+                        ),
+                        "extra-topic-1": TopicConfig(
+                            role="first-extra-topic",
+                            partitions_count=10,
+                        ),
+                    }
+                },
+            },
+        )
+
+        assert producer_app.values.kafka.output_topic == KafkaTopic(
+            name="producer-app-output-topic"
+        )
+        assert producer_app.values.kafka.labeled_output_topics == {
+            "first-extra-topic": KafkaTopic(name="extra-topic-1")
+        }
+
+    @pytest.mark.asyncio()
+    async def test_deploy_order_when_dry_run_is_false(
+        self,
+        producer_app: ProducerAppV3,
+        mocker: MockerFixture,
+    ):
+        mock_create_topic = mocker.patch.object(
+            get_handlers().topic_handler, "create_topic"
+        )
+
+        mock_helm_upgrade_install = mocker.patch.object(
+            producer_app.helm, "upgrade_install"
+        )
+
+        mock = mocker.AsyncMock()
+        mock.attach_mock(mock_create_topic, "mock_create_topic")
+        mock.attach_mock(mock_helm_upgrade_install, "mock_helm_upgrade_install")
+
+        await producer_app.deploy(dry_run=False)
+        assert producer_app.to
+        assert mock.mock_calls == [
+            *(
+                mocker.call.mock_create_topic(topic, dry_run=False)
+                for topic in producer_app.to.kafka_topics
+            ),
+            mocker.call.mock_helm_upgrade_install(
+                PRODUCER_APP_RELEASE_NAME,
+                "bakdata-streams-bootstrap/producer-app",
+                False,
+                "test-namespace",
+                {
+                    "nameOverride": PRODUCER_APP_HELM_NAME_OVERRIDE,
+                    "kafka": {
+                        "bootstrapServers": "fake-broker:9092",
+                        "outputTopic": "producer-app-output-topic",
+                    },
+                },
+                HelmUpgradeInstallFlags(
+                    force=False,
+                    username=None,
+                    password=None,
+                    ca_file=None,
+                    insecure_skip_tls_verify=False,
+                    timeout="5m0s",
+                    version="3.2.1",
+                    wait=True,
+                    wait_for_jobs=False,
+                ),
+            ),
+        ]
+
+    @pytest.mark.asyncio()
+    async def test_destroy(
+        self,
+        producer_app: ProducerAppV3,
+        mocker: MockerFixture,
+    ):
+        mock_helm_uninstall = mocker.patch.object(producer_app.helm, "uninstall")
+
+        await producer_app.destroy(dry_run=True)
+
+        mock_helm_uninstall.assert_called_once_with(
+            "test-namespace", PRODUCER_APP_RELEASE_NAME, True
+        )
+
+    @pytest.mark.asyncio()
+    async def test_should_clean_producer_app(
+        self,
+        producer_app: ProducerAppV3,
+        empty_helm_get_values: MockerFixture,
+        mocker: MockerFixture,
+    ):
+        # actual component
+        mock_helm_uninstall_producer_app = mocker.patch.object(
+            producer_app.helm, "uninstall"
+        )
+
+        # cleaner
+        mock_helm_upgrade_install = mocker.patch.object(
+            producer_app._cleaner.helm, "upgrade_install"
+        )
+        mock_helm_uninstall = mocker.patch.object(
+            producer_app._cleaner.helm, "uninstall"
+        )
+        mock_helm_print_helm_diff = mocker.patch.object(
+            producer_app._cleaner.dry_run_handler, "print_helm_diff"
+        )
+
+        mock = mocker.MagicMock()
+        mock.attach_mock(
+            mock_helm_uninstall_producer_app, "helm_uninstall_producer_app"
+        )
+        mock.attach_mock(mock_helm_uninstall, "helm_uninstall")
+        mock.attach_mock(mock_helm_upgrade_install, "helm_upgrade_install")
+        mock.attach_mock(mock_helm_print_helm_diff, "print_helm_diff")
+
+        await producer_app.clean(dry_run=True)
+
+        mock.assert_has_calls(
+            [
+                mocker.call.helm_uninstall_producer_app(
+                    "test-namespace", PRODUCER_APP_RELEASE_NAME, True
+                ),
+                ANY,  # __bool__
+                ANY,  # __str__
+                mocker.call.helm_uninstall(
+                    "test-namespace",
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    True,
+                ),
+                ANY,  # __bool__
+                ANY,  # __str__
+                mocker.call.helm_upgrade_install(
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    "bakdata-streams-bootstrap/producer-app-cleanup-job",
+                    True,
+                    "test-namespace",
+                    {
+                        "nameOverride": PRODUCER_APP_CLEAN_HELM_NAMEOVERRIDE,
+                        "kafka": {
+                            "bootstrapServers": "fake-broker:9092",
+                            "outputTopic": "producer-app-output-topic",
+                        },
+                    },
+                    HelmUpgradeInstallFlags(
+                        version="3.2.1", wait=True, wait_for_jobs=True
+                    ),
+                ),
+                mocker.call.print_helm_diff(
+                    ANY,
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    logging.getLogger("HelmApp"),
+                ),
+                mocker.call.helm_uninstall(
+                    "test-namespace",
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    True,
+                ),
+                ANY,  # __bool__
+                ANY,  # __str__
+            ]
+        )
+
+    @pytest.mark.asyncio()
+    async def test_should_clean_producer_app_and_deploy_clean_up_job_and_delete_clean_up_with_dry_run_false(
+        self,
+        mocker: MockerFixture,
+        producer_app: ProducerAppV3,
+        empty_helm_get_values: MockerFixture,
+    ):
+        # actual component
+        mock_helm_uninstall_producer_app = mocker.patch.object(
+            producer_app.helm, "uninstall"
+        )
+
+        # cleaner
+        mock_helm_upgrade_install = mocker.patch.object(
+            producer_app._cleaner.helm, "upgrade_install"
+        )
+        mock_helm_uninstall = mocker.patch.object(
+            producer_app._cleaner.helm, "uninstall"
+        )
+
+        mock = mocker.MagicMock()
+        mock.attach_mock(
+            mock_helm_uninstall_producer_app, "helm_uninstall_producer_app"
+        )
+        mock.attach_mock(mock_helm_upgrade_install, "helm_upgrade_install")
+        mock.attach_mock(mock_helm_uninstall, "helm_uninstall")
+
+        await producer_app.clean(dry_run=False)
+
+        mock.assert_has_calls(
+            [
+                mocker.call.helm_uninstall_producer_app(
+                    "test-namespace", PRODUCER_APP_RELEASE_NAME, False
+                ),
+                ANY,  # __bool__
+                ANY,  # __str__
+                mocker.call.helm_uninstall(
+                    "test-namespace",
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    False,
+                ),
+                ANY,  # __bool__
+                ANY,  # __str__
+                mocker.call.helm_upgrade_install(
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    "bakdata-streams-bootstrap/producer-app-cleanup-job",
+                    False,
+                    "test-namespace",
+                    {
+                        "nameOverride": PRODUCER_APP_CLEAN_HELM_NAMEOVERRIDE,
+                        "kafka": {
+                            "bootstrapServers": "fake-broker:9092",
+                            "outputTopic": "producer-app-output-topic",
+                        },
+                    },
+                    HelmUpgradeInstallFlags(
+                        version="3.2.1", wait=True, wait_for_jobs=True
+                    ),
+                ),
+                mocker.call.helm_uninstall(
+                    "test-namespace",
+                    PRODUCER_APP_CLEAN_RELEASE_NAME,
+                    False,
+                ),
+                ANY,  # __bool__
+                ANY,  # __str__
+            ]
+        )
+
+    def test_get_output_topics(self):
+        producer_app = ProducerAppV3(
+            name="my-producer",
+            **{
+                "namespace": "test-namespace",
+                "values": {
+                    "namespace": "test-namespace",
+                    "kafka": {"bootstrapServers": "fake-broker:9092"},
+                },
+                "to": {
+                    "topics": {
+                        "producer-app-output-topic": TopicConfig(
+                            type=OutputTopicTypes.OUTPUT, partitions_count=10
+                        ),
+                        "extra-topic-1": TopicConfig(
+                            role="first-extra-topic",
+                            partitions_count=10,
+                        ),
+                    }
+                },
+            },
+        )
+        assert producer_app.values.kafka.output_topic == KafkaTopic(
+            name="producer-app-output-topic"
+        )
+        assert producer_app.values.kafka.labeled_output_topics == {
+            "first-extra-topic": KafkaTopic(name="extra-topic-1")
+        }
+        assert producer_app.input_topics == []
+        assert list(producer_app.inputs) == []
+        assert list(producer_app.outputs) == [
+            KafkaTopic(name="producer-app-output-topic"),
+            KafkaTopic(name="extra-topic-1"),
+        ]
+
+    @pytest.mark.asyncio()
+    async def test_should_not_deploy_clean_up_when_rest(self, mocker: MockerFixture):
+        image_tag_in_cluster = "1.1.1"
+        mocker.patch.object(
+            Helm,
+            "get_values",
+            return_value={
+                "image": "registry/producer-app",
+                "imageTag": image_tag_in_cluster,
+                "nameOverride": PRODUCER_APP_NAME,
+                "replicaCount": 1,
+                "kafka": {
+                    "bootstrapServers": "fake-broker:9092",
+                    "outputTopic": "test-output-topic",
+                    "schemaRegistryUrl": "http://localhost:8081",
+                },
+            },
+        )
+        producer_app = ProducerAppV3(
+            name=PRODUCER_APP_NAME,
+            **{
+                "namespace": "test-namespace",
+                "values": {
+                    "imageTag": "2.2.2",
+                    "kafka": {"bootstrapServers": "fake-broker:9092"},
+                },
+                "to": {
+                    "topics": {
+                        "test-output-topic": {"type": "output"},
+                    }
+                },
+            },
+        )
+        uninstall_producer_mock = mocker.patch.object(producer_app.helm, "uninstall")
+        mocker.patch.object(producer_app._cleaner.dry_run_handler, "print_helm_diff")
+        mocker.patch.object(producer_app._cleaner.helm, "uninstall")
+
+        mock_helm_upgrade_install_clean_up = mocker.patch.object(
+            producer_app._cleaner.helm, "upgrade_install"
+        )
+
+        dry_run = True
+        await producer_app.reset(dry_run)
+        uninstall_producer_mock.assert_called_once_with(
+            "test-namespace", PRODUCER_APP_RELEASE_NAME, dry_run
+        )
+        mock_helm_upgrade_install_clean_up.assert_not_called()
+
+    @pytest.mark.asyncio()
+    async def test_should_deploy_clean_up_job_with_values_in_cluster_when_clean(
+        self, mocker: MockerFixture
+    ):
+        image_tag_in_cluster = "1.1.1"
+        mocker.patch.object(
+            Helm,
+            "get_values",
+            return_value={
+                "image": "registry/producer-app",
+                "imageTag": image_tag_in_cluster,
+                "nameOverride": PRODUCER_APP_NAME,
+                "replicaCount": 1,
+                "kafka": {
+                    "bootstrapServers": "fake-broker:9092",
+                    "outputTopic": "test-output-topic",
+                    "schemaRegistryUrl": "http://localhost:8081",
+                },
+            },
+        )
+        producer_app = ProducerAppV3(
+            name=PRODUCER_APP_NAME,
+            **{
+                "namespace": "test-namespace",
+                "values": {
+                    "imageTag": "2.2.2",
+                    "kafka": {"bootstrapServers": "fake-broker:9092"},
+                },
+                "to": {
+                    "topics": {
+                        "test-output-topic": {"type": "output"},
+                    }
+                },
+            },
+        )
+        mocker.patch.object(producer_app.helm, "uninstall")
+        mocker.patch.object(producer_app._cleaner.dry_run_handler, "print_helm_diff")
+        mocker.patch.object(producer_app._cleaner.helm, "uninstall")
+
+        mock_helm_upgrade_install = mocker.patch.object(
+            producer_app._cleaner.helm, "upgrade_install"
+        )
+
+        dry_run = True
+        await producer_app.clean(dry_run)
+
+        mock_helm_upgrade_install.assert_called_once_with(
+            PRODUCER_APP_CLEAN_RELEASE_NAME,
+            "bakdata-streams-bootstrap/producer-app-cleanup-job",
+            dry_run,
+            "test-namespace",
+            {
+                "image": "registry/producer-app",
+                "nameOverride": PRODUCER_APP_CLEAN_HELM_NAMEOVERRIDE,
+                "imageTag": image_tag_in_cluster,
+                "replicaCount": 1,
+                "kafka": {
+                    "bootstrapServers": "fake-broker:9092",
+                    "outputTopic": "test-output-topic",
+                    "schemaRegistryUrl": "http://localhost:8081",
+                },
+            },
+            HelmUpgradeInstallFlags(version="3.0.0", wait=True, wait_for_jobs=True),
+        )
