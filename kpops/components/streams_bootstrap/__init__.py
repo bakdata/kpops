ac1ca1112a8211bf96e405f98271d3e7fc83c393
Salomon Popp <salomon.popp@bakdata.com>
2025-01-07 15:52:39 +0100

    Apply Ruff fixes

--- kpops/components/streams_bootstrap/__init__.py
+++ kpops/components/streams_bootstrap/__init__.py
@@ -3,4 +3,4 @@ from kpops.components.streams_bootstrap.base import StreamsBootstrap
 from .producer.producer_app import ProducerApp
 from .streams.streams_app import StreamsApp
 
-__all__ = ("StreamsBootstrap", "StreamsApp", "ProducerApp")
+__all__ = ("ProducerApp", "StreamsApp", "StreamsBootstrap")
