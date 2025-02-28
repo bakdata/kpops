c1a493099660cd84b0defd47a5c5edaa04a136f9
Salomon Popp <salomon.popp@bakdata.com>
2025-02-28 18:46:46 +0100

    test: fix import path

--- tests/component_handlers/schema_handler/test_schema_handler.py
+++ tests/component_handlers/schema_handler/test_schema_handler.py
@@ -4,7 +4,6 @@ from unittest import mock
 from unittest.mock import AsyncMock, MagicMock
 
 import pytest
-from pipeline.test_components.components import TestSchemaProvider
 from pydantic import AnyHttpUrl, TypeAdapter
 from pytest_mock import MockerFixture
 from schema_registry.client.schema import AvroSchema
@@ -19,6 +18,7 @@ from kpops.components.base_components.models.to_section import (
 from kpops.components.common.topic import OutputTopicTypes, TopicConfig
 from kpops.config import KpopsConfig, SchemaRegistryConfig
 from kpops.utils.colorify import greenify, magentaify, yellowify
+from tests.pipeline.test_components.components import TestSchemaProvider
 
 log = logging.getLogger("SchemaHandler")
 
