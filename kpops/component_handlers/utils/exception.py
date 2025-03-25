99bc5396a2d840ff1ad94eb90b4f4154f17739f7
Salomon Popp <salomon.popp@bakdata.com>
2025-03-25 17:49:55 +0100

    test(kafka-connect-api): improve & speed up tests (#618)
    related to #274
    tests run ~15s faster

--- kpops/component_handlers/utils/exception.py
+++ kpops/component_handlers/utils/exception.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import httpx
@@ -12,7 +13,7 @@ class HttpxException(Exception):
         log_lines = [f"The request responded with the code {self.error_code}."]
         if response.headers.get("Content-Type") == "application/json":
             log_lines.append("Error body:")
-            log_lines.append(response.json())
+            log_lines.append(json.dumps(response.json(), indent=2))
         try:
             response.raise_for_status()
         except httpx.HTTPError as e:
