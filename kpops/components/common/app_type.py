bdf59182b36b82e929b0c0d785ca11ec7062a4f7
Jonas Arnhold <jonas.arnhold@bakdata.com>
2026-02-24 16:16:33 +0100

    feat: add consumer app

--- kpops/components/common/app_type.py
+++ kpops/components/common/app_type.py
@@ -4,5 +4,7 @@ from enum import Enum
 class AppType(Enum):
     STREAMS_APP = "streams-app"
     PRODUCER_APP = "producer-app"
+    CONSUMER_APP = "consumer-app"
     CLEANUP_STREAMS_APP = "streams-app-cleanup-job"
     CLEANUP_PRODUCER_APP = "producer-app-cleanup-job"
+    CLEANUP_CONSUMER_APP = "consumer-app-cleanup-job"
